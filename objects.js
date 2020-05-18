c863bbb2a803f679b32448c199484f67e93f8a9e
Shan Siddiqui <shansiddiqui94@gmail.com>
2020-05-18 16:27:26 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,16 @@
+var playlist = {
+  Fray: 'Found Me'
+  }
+
+  function updatePlaylist(playlist, artistName, songTitle){
+    playlist['Slowdive'] = 'Alison'
+    playlist['My Bloody Valentine'] = 'Sometimes'
+    playlist['Phil Ochs'] = 'Here/s to the State of Mississippi'
+        // This is braket Notation and we use it to assign key-value pairs
+    return playlist
+  }
+
+  function removeFromPlaylist(playlist, artistName){
+    delete playlist.artistName
+    
+  }
