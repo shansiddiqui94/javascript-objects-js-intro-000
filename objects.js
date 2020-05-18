b4fe59c19a7cec176a7130fa4163e14e14390676
Shan Siddiqui <shansiddiqui94@gmail.com>
2020-05-18 16:30:15 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,7 +7,6 @@ var playlist = {
     playlist['My Bloody Valentine'] = 'Sometimes'
     playlist['Phil Ochs'] = 'Here/s to the State of Mississippi'
         // This is braket Notation and we use it to assign key-value pairs
-    return playlist
 
   function removeFromPlaylist(playlist, artistName){
    delete playlist.artistName;
