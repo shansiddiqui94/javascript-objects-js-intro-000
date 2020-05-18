25536680dfdbc31366bf8008d8c777d90a8df5a4
Shan Siddiqui <shansiddiqui94@gmail.com>
2020-05-18 16:30:00 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -8,7 +8,7 @@ var playlist = {
     playlist['Phil Ochs'] = 'Here/s to the State of Mississippi'
         // This is braket Notation and we use it to assign key-value pairs
     return playlist
-  
+
   function removeFromPlaylist(playlist, artistName){
    delete playlist.artistName;
    return playlist
